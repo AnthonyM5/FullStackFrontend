6e022bed681a5fdeab3ddea029ec7d84f84ab75b
AnthonyM5 <anthony.mai530@gmail.com>
2021-08-15 22:18:41 -0400

    rendering gif results

--- src/Components/gifs.js
+++ src/Components/gifs.js
@@ -1,11 +1,12 @@
 import React from "react";
+import '../images.css';
 
 
 const gifs = (props) => {
     console.log(props.data.images.downsized_large)
     return (
         <>
-            <img id={props.data.id} src={props.data.images.downsized_large.url} alt="slug"/>
+            <img className="gif" id={props.data.id} src={props.data.images.downsized_large.url} alt="slug"/>
         </>
     )
 }
